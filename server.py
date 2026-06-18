550ea94780111edc6b0c3de532e8c4a00aabb9e2
Katherine Varela <katherinevarela775@gmail.com>
2026-06-18 01:00:41 -0300

    feat(servidor): implementar ciclo de vida y cierre ordenado del servidor

--- server.py
+++ server.py
@@ -56,3 +56,42 @@ def manejar_cliente(socket_cliente, direccion): #Esta función se ejecuta en un 
         print(f"⚠️ Error con el cliente {nombre}: {e}")
     finally: 
         remover_cliente(socket_cliente) 
+
+def remover_cliente(socket_cliente):
+    if socket_cliente in clientes_conectados: 
+        nombre = clientes_conectados[socket_cliente]
+        del clientes_conectados[socket_cliente] 
+        socket_cliente.close() 
+        broadcast(f"🚶 {nombre} ha abandonado el chat.") 
+
+def iniciar_servidor():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    
+    server.settimeout(1.0) #establece timeout de 1 segundo para operaciones bloqueantes
+    
+    try:
+        server.bind((IP_SERVIDOR, PUERTO)) # Asigna dirección local al socket, 127.0.0.1: loopback interface (solo conexiones locales), 5000: puerto 
+        server.listen() # Convierte al socket en pasivo (modo servidor)
+        print("🚀 Servidor iniciado en 127.0.0.1:5000")
+        print("Interruptor: Presiona Ctrl+C para apagar el servidor de forma segura.")
+        
+        while True:
+            try:
+                cl_socket, cl_address = server.accept() #Bloquea hasta que un cliente se conecta
+                thread = threading.Thread(target=manejar_cliente, args=(cl_socket, cl_address))
+                thread.daemon = True #te aseguras de que en el momento que tú apagues el servidor, todas las conexiones se corten de inmediato
+                thread.start()
+
+            except socket.timeout: #evita que se quede congelado, permitiéndote apagarlo de forma segura en cualquier momento aunque no haya nadie conectado.
+                continue
+
+    except KeyboardInterrupt:
+        print("\n\n🛑 Apagando el servidor...")
+    finally:
+        server.close() 
+        print("✅ Puerto 5000 liberado. ¡Hasta luego!")
+
+if __name__ == "__main__": #Evita que el servidor se encienda accidentalmente si solo quisieras importar una función de ese archivo en otro proyecto.
+    iniciar_servidor()
